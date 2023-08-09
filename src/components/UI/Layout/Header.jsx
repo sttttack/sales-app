65308ac917655e7ae0b1a1b8cc30e2775d863b3e
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-08-08 23:48:17 -0700

    installed react router

--- src/components/UI/Layout/Header.jsx
+++ src/components/UI/Layout/Header.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>CarSales</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Car Collection</li>
+          <li>Location</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <div>
+        <span>+1 917 414 6014</span>
+      </div>
+    </div>
+  );
 }
