3961be105140a8066a75729f91c880626b0c595d
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-08-09 00:32:02 -0700

    Navbar decoration

--- src/components/UI/Layout/Header.jsx
+++ src/components/UI/Layout/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import CarLogo from "../../../assets/car.png";
 
 import classes from "../Layout/Header.module.css";
 import { NavLink } from "react-router-dom";
@@ -6,17 +6,30 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <div className={classes.header}>
-      <div>
-        <h1>CarSales</h1>
+      <div className={classes.logo}>
+        <img src={CarLogo} />
+        <h1>Mobile</h1>
       </div>
       <nav>
         <ul className={classes.navList}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/services">Services</NavLink>
-          <NavLink to="/carcollection">Car Collection</NavLink>
-          <NavLink to="/location">Location</NavLink>
-          <NavLink to="/services">Services</NavLink>
+          <NavLink to="/" className={classes.navLinks}>
+            Home
+          </NavLink>
+          <NavLink to="/about" className={classes.navLinks}>
+            About
+          </NavLink>
+          <NavLink to="/services" className={classes.navLinks}>
+            Services
+          </NavLink>
+          <NavLink to="/carcollection" className={classes.navLinks}>
+            Car Collection
+          </NavLink>
+          <NavLink to="/location" className={classes.navLinks}>
+            Location
+          </NavLink>
+          <NavLink to="/services" className={classes.navLinks}>
+            Services
+          </NavLink>
         </ul>
       </nav>
       <div>
