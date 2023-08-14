eb0209aad330015169ad69afff48cb1370697138
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-08-14 01:34:04 -0700

    about component style

--- src/components/UI/AboutSection/AboutSection.jsx
+++ src/components/UI/AboutSection/AboutSection.jsx
@@ -7,11 +7,26 @@ import icon3 from "../../../assets/3.png";
 import classes from "../AboutSection/AboutSection.module.css";
 
 export default function AboutSection(props) {
+  const cardText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
+
   return (
-    <div className={classes.cardSection}>
-      <Card image={icon1} heading={"Secure"} text={"lorem"} />
-      <Card image={icon2} heading={"Secure"} text={"lorem"} />
-      <Card image={icon3} heading={"Secure"} text={"lorem"} />
+    <div className={classes.main}>
+      <div className={classes.heading}>
+        <span>About</span>
+        <h1>About the Mobile</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea.
+        </p>
+      </div>
+      <div className={classes.cardSection}>
+        <Card image={icon3} heading={"Exclusive"} text={cardText} />
+        <Card image={icon1} heading={"Update"} text={cardText} />
+        <Card image={icon2} heading={"Secure"} text={cardText} />
+      </div>
     </div>
   );
 }
