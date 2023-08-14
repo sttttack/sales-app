0317283352d9a9fbde708aedb938c1d3819c708f
Papuna Maglaperidze <papunamaglaperidze@Papunas-MBP.lan>
2023-08-14 01:40:59 -0700

    added overlay

--- src/components/UI/AboutSection/AboutSection.jsx
+++ src/components/UI/AboutSection/AboutSection.jsx
@@ -12,6 +12,7 @@ export default function AboutSection(props) {
 
   return (
     <div className={classes.main}>
+      <div className={classes.sideDiv}></div>
       <div className={classes.heading}>
         <span>About</span>
         <h1>About the Mobile</h1>
